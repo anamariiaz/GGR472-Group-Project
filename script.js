de7317ed45998147b9880bd8f0a98a5dca227e1d
Ana-Maria Zamrii <anamaria.zamrii@mail.utoronto.ca>
2023-03-01 19:47:07 -0500

    fixed map?

--- script.js
+++ script.js
@@ -14,63 +14,64 @@ const map = new mapboxgl.Map({
     center: [-79.3, 43.765], //starting position [longitude, latitude]
     zoom: 9.2, //starting zoom
     bearing: -17.7, //angle rotation of map
-	maxBounds: maxBounds //maximum and minimum scroll bounds
+	//maxBounds: maxBounds //maximum and minimum scroll bounds
 });
-
-function updateMap(data) {
-    var test = [];
-    for (let step = 0; step < 652; step++) {
-        console.log(data.data.stations[step].lon)
-        let longitude=data.data.stations[step].lon
-        let latitude=data.data.stations[step].lat
-        test.push(`{"type": "Feature", "geometry": {"type": "Point", "coordinates": [${longitude},${latitude}]}}`);
-    };
-    console.log(test)
-}
-
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr.send();
-};
-getJSON('https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information',
-function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-    console.log(data);
-    //console.log(data.data.stations[1].lon)};
-    updateMap(data)}
-  });
-
 
 
 map.on('load', () => {
-
+    var getJSON = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+          var status = xhr.status;
+          if (status === 200) {
+            callback(null, xhr.response);
+          } else {
+            callback(status, xhr.response);
+          }
+        };
+        xhr.send();
+    };
+    getJSON('https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information',
+    function(err, data) {
+      if (err !== null) {
+        alert('Something went wrong: ' + err);
+      } else {
+        //console.log(data);
+        //console.log(data.data.stations[1].lon)};
+        updateMap(data)}
+        
+      });
+    
+    function updateMap(data) {
+        var test = [];
+        for (let step = 0; step < 652; step++) {
+            //console.log(data.data.stations[step].lon)
+            let longitude=data.data.stations[step].lon
+            let latitude=data.data.stations[step].lat
+            test.push(`{"type": "Feature", "geometry": {"type": "Point", "coordinates": [${longitude},${latitude}]}}`);
+        };  
+        map.addSource('bikeways', {
+            type: 'geojson',
+            data:  {"type": "FeatureCollection",
+            "features": test}
+        });
+        console.log({"type": "FeatureCollection",
+        "features": test})
+        console.log(test)
+    
+        //add and style a layer of lines "bike" from the defined "bikeways" source
+        map.addLayer({
+            'id': 'bike',
+            'type': 'circle',
+            'source': 'bikeways',
+            'paint': {
+                'circle-radius': 10,
+                'circle-color': '#000000'
+            }
+        });
+    }
     //add a geojson file source "bikeways" for Toronto bikeways
-    map.addSource('bikeways', {
-        type: 'geojson',
-        data:  {"type": "FeatureCollection",
-        "features": test}
-    });
 
-    //add and style a layer of lines "bike" from the defined "bikeways" source
-    map.addLayer({
-        'id': 'bike',
-        'type': 'circle',
-        'source': 'bikeways',
-        'paint': {
-            'circle-radius': 1,
-            'circle-color': '#B42222'
-        }
-    });
 });
